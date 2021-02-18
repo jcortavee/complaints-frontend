2a07a0b9061ceeb53426424c3d5c9d0c2c31628f
Jose Carlos Ramírez <jccortavee@gmail.com>
2021-02-18 00:09:45 -0600

    Initial configuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import NewComplaint from "./components/complaints/NewComplaint";
+import ListComplaints from "./components/complaints/ListComplaints";
+import ShowComplaint from "./components/complaints/ShowComplaint";
+import ListServiceTypes from "./components/service-types/ListServiceTypes";
+import NewServiceType from "./components/service-types/NewServiceType";
+import EditServiceType from "./components/service-types/EditServiceType";
+import Login from "./components/auth/Login";
+import ListUser from "./components/users/ListUser";
+import NewUser from "./components/users/NewUser";
+import GuardRoute from "./components/GuardRoute";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useState } from "react";
 
 function App() {
+  const[isAutheticated, setIsAutheticated] = useState(false);
+  const token = localStorage.getItem('accessToken');
+
+  if (token && !isAutheticated) {
+    setIsAutheticated(true);
+  }
+
+  if (!token && isAutheticated) {
+    setIsAutheticated(false);
+  }
+  
+  const authenticate = (isAutheticated) => {
+    setIsAutheticated(isAutheticated);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <Navbar authenticate={authenticate} isAuthenticate={isAutheticated} />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/login">
+              <Login authenticate={authenticate} />
+            </Route>
+
+            <Route exact path="/users">
+              <ListUser />
+            </Route>
+            <GuardRoute exact path="/users/new" auth={isAutheticated}>
+              <NewUser />
+            </GuardRoute>
+
+            <Route exact path="/complaint">
+              <ListComplaints />
+            </Route>
+            <Route exact path="/complaint/new">
+              <NewComplaint />
+            </Route>
+            <Route exact path="/complaint/:id">
+              <ShowComplaint />
+            </Route>
+
+            <GuardRoute exact path="/service-types" auth={isAutheticated}>
+              <ListServiceTypes />
+            </GuardRoute>
+            <Route exact path="/service-types/new">
+              <NewServiceType />
+            </Route>
+            <Route exact path="/service-types/:id">
+              <EditServiceType />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
